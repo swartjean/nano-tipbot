a2cc9f7471144bd8a12a3ddb2e2a5f8071bb476b
Unknown <swrjea@gmail.com>
2018-02-01 18:48:35 +0200

    Updated to reflect Nano rebrand
    - Changed all instances of RaiBlocks to Nano and updated XRB currency symbols accordingly.
    - Swapped out coinmarketcap library to direct API request.

--- xrb_tipbot.py
+++ xrb_tipbot.py
@@ -2,7 +2,7 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler
 import logging
-import coinmarketcap
+import requests
 import settings
 import dataset
 from io import BytesIO
@@ -20,13 +20,17 @@ logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 # Set up logging file handler
 formatter = logging.Formatter('%(asctime)s - %(message)s')
-handler = logging.FileHandler('xrb_tipbot.log')
+handler = logging.FileHandler('nano_tipbot.log')
 handler.setLevel(logging.INFO)
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 # Get the wallet from the local settings
 wallet = settings.wallet
+
+# Setup the initial Nano market statistics
+nano_market_data = requests.get('https://api.coinmarketcap.com/v1/ticker/raiblocks/')
+PriceInfo = nano_market_data.json()
 
 
 # Routine to parse the request for the rai node and retrieve the response
@@ -47,13 +51,6 @@ def communicate_wallet(wallet_command):
     return parsed_json
 
 
-# Create the CoinMarketCap object
-market = coinmarketcap.Market()
-
-# Get the initial RaiBlocks price info
-PriceInfo = market.ticker('raiblocks')
-
-
 # Function to check if the username of a client has been registered in the database
 def check_username(username):
     localdb = dataset.connect('sqlite:///' + settings.local_db_name)
@@ -68,26 +65,26 @@ def send_username_error(bot, update):
 
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Welcome to the RaiBlocks Telegram Tipbot!')
+    bot.send_message(chat_id=update.message.chat_id, text='Welcome to the Nano Telegram Tipbot!')
     help(bot, update)
 
 
 def help(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='The commands available are: \n\n'
                                                           '/help - Display this message \n'
-                                                          '/price - Display current RaiBlocks market statistics \n'
-                                                          '/register - Sign up for the XRB Tipbot \n'
+                                                          '/price - Display current Nano market statistics \n'
+                                                          '/register - Sign up for the Nano Tipbot \n'
                                                           '/balance - Check your Tipbot address balance \n'
                                                           '/deposit - Get your deposit address \n'
                                                           '/recover <recovery key> - Enter a recovery key to restore access to your account in the event of a username change \n'
-                                                          '/tip <username> <amount> - Tip a user some XRB! They must already be registered on the Tipbot \n'
+                                                          '/tip <username> <amount> - Tip a user some Nano! They must already be registered on the Tipbot \n'
                                                           '/withdraw <address> <amount> - Withdraw your Tipbot funds to your personal wallet \n'
-                                                          '/contribute - Find out how to contribute to the running of the XRB Tipbot')
+                                                          '/contribute - Find out how to contribute to the running of the Nano Tipbot')
 
 
 def price(bot, update):
     # Display the currently storecd info from CoinMarketCap
-    bot.send_message(chat_id=update.message.chat_id, text='The current RaiBlocks market statistics are: \n'
+    bot.send_message(chat_id=update.message.chat_id, text='The current Nano market statistics are: \n'
                                                           'Market Cap (USD): $' + PriceInfo[0]['market_cap_usd'] + '\n'
                                                           'Price (USD): $' + PriceInfo[0]['price_usd'] + '\n'
                                                           'Price (BTC): ' u'\u0243' + PriceInfo[0]['price_btc'] + '\n'
@@ -117,12 +114,12 @@ def register(bot, update):
         recoverykey = str(uuid.uuid4())
         # Insert row containing username, account and recovery key into database
         logger.info('Registered user ' + client_username + ' with address ' + address + ' and recovery key ' + recoverykey)
-        users_table.insert(dict(user_id=client_username, xrb_address=str(address), recovery_key=recoverykey))
+        users_table.insert(dict(user_id=client_username, nano_address=str(address), recovery_key=recoverykey))
         # Notify the user
         bot.send_message(chat_id=update.message.chat_id, text='You have been registered! \n\n'
                                                               'Your deposit address is: ' + address + '\n\n'
                                                               'Your recovery key is: ' + recoverykey + '\n\n'
-                                                              'Your username is your key to accessing your stored funds in the XRB Tipbot. '
+                                                              'Your username is your key to accessing your stored funds in the Nano Tipbot. '
                                                               'In order to avoid losing access to your funds or potential unauthorized access, please do not '
                                                               'change your username before withdrawing all funds.\n\n'
                                                               'Should you lose or change your username and lose access to your funds, please use /recover <recovery key> to authorize '
@@ -142,7 +139,7 @@ def balance(bot, update):
 
     if not client_info:
         # If the user does not exist in the database they must register before checking their balance
-        bot.send_message(chat_id=update.message.chat_id, text='You have not yet been registered for the XRB Tipbot.\n\n'
+        bot.send_message(chat_id=update.message.chat_id, text='You have not yet been registered for the Nano Tipbot.\n\n'
                                                               'Please use /register to sign up.')
     else:
         # The client has a valid username and they are already registered
@@ -153,12 +150,12 @@ def balance(bot, update):
         wallet_output = communicate_wallet(wallet_command)
         wallet_command = {'action': 'rai_from_raw', 'amount': int(wallet_output['balance'])}
         rai_balance = communicate_wallet(wallet_command)
-        xrb_balance = format((float(rai_balance['amount']) / 1000000.0), '.6f')
-        # Send the account balance in XRB to the user:
-        bot.send_message(chat_id=update.message.chat_id, text='Your XRB Tipbot address is: \n\n' +
+        nano_balance = format((float(rai_balance['amount']) / 1000000.0), '.6f')
+        # Send the account balance in Nano to the user:
+        bot.send_message(chat_id=update.message.chat_id, text='Your Nano Tipbot address is: \n\n' +
                                                               address + '\n\n'
                                                               'Your balance is: \n\n'
-                                                              'XRB ' + str(xrb_balance))
+                                                              'Nano ' + str(nano_balance))
 
 
 def recover(bot, update):
@@ -171,7 +168,7 @@ def recover(bot, update):
         send_username_error(bot, update)
     elif client_info:
         # If the user already exists in the database, notify them and show them their current recovery key.
-        bot.send_message(chat_id=update.message.chat_id, text='Your username is already associated with an address on XRB Tipbot.\n\n'
+        bot.send_message(chat_id=update.message.chat_id, text='Your username is already associated with an address on Nano Tipbot.\n\n'
                                                               'Your recovery key is ' + client_info['recovery_key'])
     else:
         # The client has a username and they are not yet registered, so begin the recovery process
@@ -214,7 +211,7 @@ def tip(bot, update):
 
         if not client_info:
             # If the user does not exist in the database they must register before checking their balance
-            bot.send_message(chat_id=update.message.chat_id, text='You have not yet been registered for the XRB Tipbot.\n\n'
+            bot.send_message(chat_id=update.message.chat_id, text='You have not yet been registered for the Nano Tipbot.\n\n'
                                                                   'Please use /register to sign up.')
         else:
             # Check to ensure that the tip command has the correct format - "/tip username amount"
@@ -222,12 +219,12 @@ def tip(bot, update):
             if len(update.message.text.split(' ')) == 1:
                 bot.send_message(chat_id=update.message.chat_id, text='Please use /tip <username> <amount> to tip a user.\n\n'
                                                                       'Example:\n'
-                                                                      '/tip xrb_tipbot 0.001')
+                                                                      '/tip nano_tipbot 0.001')
             elif len(update.message.text.split(' ')) != 3:
                 bot.send_message(chat_id=update.message.chat_id, text='Incorrect format. \n\n'
                                                                       'Please use /tip <username> <amount> \n\n'
                                                                       'Example:\n'
-                                                                      '/tip xrb_tipbot 0.001')
+                                                                      '/tip nano_tipbot 0.001')
             else:
                 # Check to see if the recipient is registered with the tip bot.
                 recipient_username = update.message.text.split(' ')[1]
@@ -266,7 +263,7 @@ def tip(bot, update):
                         client_address = client_info['xrb_address']
                         recipient_address = recipient_info['xrb_address']
 
-                        # Get the balance of the client to ensure that they have enough XRB in their account
+                        # Get the balance of the client to ensure that they have enough Nano in their account
                         wallet_command = {'action': 'account_balance', 'account': client_address}
                         wallet_output = communicate_wallet(wallet_command)
 
@@ -279,17 +276,17 @@ def tip(bot, update):
                         if int(rai_send_amount) <= int(client_balance['amount']):
                             wallet_command = {'action': 'send', 'wallet': wallet, 'source': client_address, 'destination': recipient_address, 'amount': int(raw_send_amount)}
                             wallet_output = communicate_wallet(wallet_command)
-                            logger.info('User ' + client_username + ' (address ' + client_address + ') sent user ' + recipient_username + ' (address ' + recipient_address + ') XRB ' + send_amount)
-                            bot.send_message(chat_id=update.message.chat_id, text='You have successfully tipped @' + recipient_username + ' with XRB ' + send_amount + '\n\n'
-                                                                                  'Thank you for using the XRB Tipbot! \n\n'
+                            logger.info('User ' + client_username + ' (address ' + client_address + ') sent user ' + recipient_username + ' (address ' + recipient_address + ') Nano ' + send_amount)
+                            bot.send_message(chat_id=update.message.chat_id, text='You have successfully tipped @' + recipient_username + ' with Nano ' + send_amount + '\n\n'
+                                                                                  'Thank you for using the Nano Tipbot! \n\n'
                                                                                   'Let @' + recipient_username + ' know that you have tipped them by sending them the message below.')
                             if new_recipient:
-                                bot.send_message(chat_id=update.message.chat_id, text='@' + recipient_username + ' has been tipped XRB ' + send_amount + ' using the XRB Tipbot (@xrb_tipbot) courtesy of @' + client_username + '.\n\n' +
-                                                                                      '@' + recipient_username + ', an account has been created for you on @xrb_tipbot where you can access your funds.')
+                                bot.send_message(chat_id=update.message.chat_id, text='@' + recipient_username + ' has been tipped Nano ' + send_amount + ' using the Nano Tipbot (@nano_tipbot) courtesy of @' + client_username + '.\n\n' +
+                                                                                      '@' + recipient_username + ', an account has been created for you on @nano_tipbot where you can access your funds.')
                             else:
-                                bot.send_message(chat_id=update.message.chat_id, text='@' + recipient_username + ' has been tipped XRB ' + send_amount + ' using the XRB Tipbot (@xrb_tipbot) courtesy of @' + client_username + '.')
+                                bot.send_message(chat_id=update.message.chat_id, text='@' + recipient_username + ' has been tipped Nano ' + send_amount + ' using the Nano Tipbot (@nano_tipbot) courtesy of @' + client_username + '.')
                         else:
-                            bot.send_message(chat_id=update.message.chat_id, text='Not enough funds to send XRB ' + send_amount + '\n\n'
+                            bot.send_message(chat_id=update.message.chat_id, text='Not enough funds to send Nano ' + send_amount + '\n\n'
                                                                                   'Please use /balance to check your account balance.')
                     except:
                         bot.send_message(chat_id=update.message.chat_id, text='Invalid amount entered: ' + send_amount)
@@ -306,7 +303,7 @@ def withdraw(bot, update):
 
         if not client_info:
             # If the user does not exist in the database they must register before checking their balance
-            bot.send_message(chat_id=update.message.chat_id, text='You have not yet been registered for the XRB Tipbot.\n\n'
+            bot.send_message(chat_id=update.message.chat_id, text='You have not yet been registered for the Nano Tipbot.\n\n'
                                                                   'Please use /register to sign up.')
         else:
             # If the user has only entered /withdraw, notify them of the correct formatting:
@@ -321,8 +318,9 @@ def withdraw(bot, update):
                 withdraw_address = update.message.text.split(' ')[1]
                 wallet_command = {"action": "validate_account_number", "account": withdraw_address}
                 address_validation = communicate_wallet(wallet_command)
-                # If the address was the incorrect length, did not start with xrb_ or was deemed invalid by the node, return an error.
-                if len(withdraw_address) != 64 or withdraw_address[:4] != "xrb_" or address_validation['valid'] != '1':
+                # If the address was the incorrect length, did not start with xrb_ or nano_ or was deemed invalid by the node, return an error.
+                address_prefix_valid = withdraw_address[:4] == "xrb_" or withdraw_address[:5] == "nano_"
+                if len(withdraw_address) != 64 or not address_prefix_valid or address_validation['valid'] != '1':
                     bot.send_message(chat_id=update.message.chat_id, text='The address: ' +
                                                                           withdraw_address + ' '
                                                                           'is not a valid address. \n\n'
@@ -335,7 +333,7 @@ def withdraw(bot, update):
                         # Get the amount to send:
                         withdraw_amount = update.message.text.split(' ')[2]
 
-                        # Get the balance of the client to ensure that they have enough XRB in their account
+                        # Get the balance of the client to ensure that they have enough Nano in their account
                         wallet_command = {'action': 'account_balance', 'account': client_address}
                         wallet_output = communicate_wallet(wallet_command)
 
@@ -348,11 +346,11 @@ def withdraw(bot, update):
                         if int(rai_withdraw_amount) <= int(client_balance['amount']):
                             wallet_command = {'action': 'send', 'wallet': wallet, 'source': client_address, 'destination': withdraw_address, 'amount': int(raw_withdraw_amount)}
                             wallet_output = communicate_wallet(wallet_command)
-                            logger.info('User ' + client_username + ' (address ' + client_address + ') withdrew XRB ' + withdraw_amount + ' to address ' + withdraw_address)
-                            bot.send_message(chat_id=update.message.chat_id, text='You have successfully withdrawn XRB ' + withdraw_amount + ' to ' + withdraw_address + '\n\n'
-                                                                                  'Thank you for using the XRB Tipbot!')
+                            logger.info('User ' + client_username + ' (address ' + client_address + ') withdrew Nano ' + withdraw_amount + ' to address ' + withdraw_address)
+                            bot.send_message(chat_id=update.message.chat_id, text='You have successfully withdrawn Nano ' + withdraw_amount + ' to ' + withdraw_address + '\n\n'
+                                                                                  'Thank you for using the Nano Tipbot!')
                         else:
-                            bot.send_message(chat_id=update.message.chat_id, text='Not enough funds to withdraw XRB ' + withdraw_amount + '\n\n'
+                            bot.send_message(chat_id=update.message.chat_id, text='Not enough funds to withdraw Nano ' + withdraw_amount + '\n\n'
                                                                                   'Please use /balance to check your account balance.')
                     except:
                         bot.send_message(chat_id=update.message.chat_id, text='Invalid amount entered: ' + withdraw_amount)
@@ -362,15 +360,16 @@ def contribute(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text='Thanks for your interest in contributing!\n\n'
                                                           'This project is run as a labour of love, but if you would like to help '
                                                           'cover my almost nonexistent server costs or buy me a coffee feel free to tip the bot:\n\n'
-                                                          '/tip xrb_tipbot <amount> \n\n'
+                                                          '/tip nano_tipbot <amount> \n\n'
                                                           'or contribute directly to:\n\n'
                                                           'xrb_1meyyw7s1kia5e368e8oyb4coydqfb6gkoroxgeofjyedohhs1g5dak8en75 \n\n'
-                                                          'Thanks for using the XRB Tipbot!')
+                                                          'Thanks for using the Nano Tipbot!')
 
 
 def update_price_info(bot, job):
-    market = coinmarketcap.Market()
-    PriceInfo = market.ticker('raiblocks')
+    global PriceInfo
+    nano_market_data = requests.get('https://api.coinmarketcap.com/v1/ticker/raiblocks/')
+    PriceInfo = nano_market_data.json()
 
 
 start_handler = CommandHandler('start', start)
